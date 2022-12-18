114f3fc6f0fd63ada405db270cd88fcd54564308
dagenduizhanga <85776844+dagenduizhanga@users.noreply.github.com>
2022-12-18 21:31:32 +0800

    Update ddqd.js

--- js/ddqd.js
+++ js/ddqd.js
@@ -1,6 +1,6 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.proEndDate = "2099-01-01T00:00:00.000+0000";
+obj.proEndDate = "2999-01-01T00:00:00.000+0000";
 obj.needSubscribe = false;
 obj.pro = true;
 body = JSON.stringify(obj);
